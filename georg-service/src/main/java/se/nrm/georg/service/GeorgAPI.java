b0e7a77b62a60686a7a24a1da5b7c757b5303a35
Ida Li <ida.li@nrm.se>
2020-02-24 09:26:40 +0100

    Add GeoAPI

--- georg-service/src/main/java/se/nrm/georg/service/GeorgAPI.java
+++ georg-service/src/main/java/se/nrm/georg/service/GeorgAPI.java
@@ -3,13 +3,16 @@ package se.nrm.georg.service;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
+import io.swagger.annotations.Tag; 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j; 
+import se.nrm.georg.service.logic.GeoCoding;
 
 /**
  *
@@ -24,15 +27,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GeorgAPI {
   
+  @Inject
+  private GeoCoding geoCoding;
+ 
   @GET
-  @Path("/run")
-  @ApiOperation(value = "Run Hello World",
+  @Path("/geoCoding")
+  @ApiOperation(value = "Get geocoding",
           notes = "Returns status",
           response = String.class
   )
   @Produces(MediaType.APPLICATION_JSON)
-  public Response run() {  
-    return Response.ok("Hello world").build();
+  public Response getGeoCode(@QueryParam("address") String address) {  
+    log.info("getGeoCode: {}, {}", address, geoCoding);
+     
+    return Response.ok(geoCoding.getGeoCode(address)).build();
   }
-  
+   
+  @GET
+  @Path("/reverse")
+  @ApiOperation(value = "Get reverse geocoding",
+          notes = "Returns status",
+          response = String.class
+  )
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getReverseGeoCode(@QueryParam("lat") double lat, 
+          @QueryParam("lng") double lon) {  
+    log.info("getReverseGeoCode: {}, {}", lat, lon);
+     
+    return Response.ok(geoCoding.getReverseGeoCode(lat, lon)).build();
+  }
 }
