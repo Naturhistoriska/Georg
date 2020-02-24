53462ed59692cc99c0929f87b493d32189f194ad
Ida Li <ida.li@nrm.se>
2020-02-24 09:27:08 +0100

    Add InitialProperties

---
+++ georg-service/src/main/java/se/nrm/georg/service/logic/InitialProperties.java
@@ -0,0 +1,37 @@
+package se.nrm.georg.service.logic;
+
+import java.io.Serializable;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
+
+/**
+ *
+ * @author idali
+ */
+@ApplicationScoped
+@Slf4j
+public class InitialProperties implements Serializable {
+
+  private final static String CONFIG_INITIALLISING_ERROR = "Property not initialized";
+  
+  private String peliasPath;
+  
+  public InitialProperties() {
+  }
+  
+  @Inject
+  public InitialProperties(@ConfigurationValue("swarm.pelias.path") String peliasPath, 
+          @ConfigurationValue("swarm.mapping-files.path") String defaultMappingFilePath) { 
+    this.peliasPath = peliasPath; 
+    log.info("test injection : {} ", peliasPath);
+  }
+  
+  public String getPeliasPath() {
+    if (peliasPath == null) {
+      throw new RuntimeException(CONFIG_INITIALLISING_ERROR);
+    }
+    return peliasPath;
+  }  
+}
