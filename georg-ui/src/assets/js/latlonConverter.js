eeb742d3a81d7e79b098be274b7debd6f4cd297f
Ida Li <ida.li@nrm.se>
2020-03-09 09:58:38 +0100

    Add latlonConverter.js for convert decimal lat and lon to degree

---
+++ georg-ui/src/assets/js/latlonConverter.js
@@ -0,0 +1,23 @@
+function truncate(n) {
+  return n > 0 ? Math.floor(n) : Math.ceil(n);
+}
+
+export function latlon(dd, longOrLat) {
+  let hemisphere = /^[WE]|(?:lon)/i.test(longOrLat)
+    ? dd < 0
+      ? "W"
+      : "E"
+    : dd < 0
+    ? "S"
+    : "N";
+
+  const absDD = Math.abs(dd);
+  const degrees = truncate(absDD);
+  const minutes = truncate((absDD - degrees) * 60);
+  const seconds = ((absDD - degrees - minutes / 60) * Math.pow(60, 2)).toFixed(
+    2
+  );
+
+  let dmsArray = [degrees, minutes, seconds, hemisphere];
+  return `${dmsArray[0]}°${dmsArray[1]}'${dmsArray[2]}" ${dmsArray[3]}`;
+}
