276e7a1e43d3a9015e32331a43c167d2ee4e20e4
Ida Li <ida.li@nrm.se>
2020-03-09 09:56:44 +0100

    Create Home and About views

---
+++ georg-ui/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page">
+    <Search v-on:search-address="doSearch" />
+    <Results v-bind:results="results" />
+  </div>
+</template>
+
+<script>
+import Search from "../components/Search";
+import Results from "../components/Results";
+
+export default {
+  name: "Home",
+  props: {},
+  components: {
+    Search,
+    Results
+  },
+  data() {
+    return {
+      results: []
+    };
+  },
+
+  methods: {
+    clearSearch() {
+      console.log("reset clicked");
+    },
+    doSearch(address) {
+      console.log("address..." + address);
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const url = `https://georg.nrm.se/geoCoding?address=${address}`;
+      // use fetch or axios?
+      fetch(proxyurl + url, {
+        // mode: "no-cors",
+        method: "get"
+      })
+        .then(response => {
+          return response.text();
+        })
+        .then(jsonData => {
+          this.results = JSON.parse(jsonData).features;
+        });
+    }
+  }
+};
+</script>
