1f4598b4b9da3971a4bb35375c7c3dc1138ef978
Ida Li <ida.li@nrm.se>
2020-03-09 09:56:10 +0100

    Create layout and Header.vue for later use

---
+++ georg-ui/src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  text-align: center;
+  color: #fff;
+  padding: 10px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
