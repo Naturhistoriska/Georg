0d9bd168f9ce09b23cad6660cf20cc53cb23507a
Ida Li <ida.li@nrm.se>
2020-03-11 13:46:12 +0100

    Update main.js

--- georg-ui/src/main.js
+++ georg-ui/src/main.js
@@ -3,24 +3,44 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import "leaflet/dist/leaflet.css";
+
+import "vuetify/dist/vuetify.css";
+// import "vue-material-design-icons/styles.css";
+
 import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "leaflet/dist/leaflet.css";
 
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { Icon } from "leaflet";
 
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "leaflet-defaulticon-compatibility";
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 
-Vue.component("v-map", LMap);
-Vue.component("v-tilelayer", LTileLayer);
-Vue.component("v-marker", LMarker);
+Vue.component("l-map", LMap);
+Vue.component("l-tile-layer", LTileLayer);
+Vue.component("l-marker", LMarker);
 
 Vue.config.productionTip = false;
+
+Vue.use(vuetify, {
+  theme: {
+    primary: "#FF6E40",
+    secondary: "#FF9E80",
+    accent: "#26A69A",
+    error: "#FF3D00",
+    warning: "#EA80FC",
+    info: "#29B6F6",
+    success: "#64DD17"
+  }
+});
 
 new Vue({
   router,
