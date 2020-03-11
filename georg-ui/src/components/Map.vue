965aa63823094853587efad51bd40405109d0819
Ida Li <ida.li@nrm.se>
2020-03-11 13:44:50 +0100

    Create Map.vue

---
+++ georg-ui/src/components/Map.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="map">
+    <l-map :zoom="4" :center="coordinates" ref="myMap">
+      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-marker :lat-lng="latlon"></l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MapTwo",
+  props: ["coordinates", "latlon"],
+
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.myMap.mapObject.invalidateSize();
+    });
+  }
+};
+</script>
+
+<style scoped>
+#map {
+  height: 400px;
+  width: 800px !important;
+  position: fixed;
+  display: block;
+  z-index: -10;
+}
+</style>
