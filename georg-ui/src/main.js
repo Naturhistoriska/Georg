5f8058c340314722a004aed6173833fdfec79fa3
Ida Li <ida.li@nrm.se>
2020-03-12 10:57:07 +0100

    Fix marker not in correct positon when map zoom out

--- georg-ui/src/main.js
+++ georg-ui/src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueAxios from "vue-axios";
-// import AxiosPlugin from "vue-axios-cors";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -8,24 +7,15 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 import "vuetify/dist/vuetify.css";
-// import "vue-material-design-icons/styles.css";
 
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import "leaflet/dist/leaflet.css";
 
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
-import { Icon } from "leaflet";
 
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
 
 import "leaflet-defaulticon-compatibility";
-
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-});
 
 Vue.component("l-map", LMap);
 Vue.component("l-tile-layer", LTileLayer);
