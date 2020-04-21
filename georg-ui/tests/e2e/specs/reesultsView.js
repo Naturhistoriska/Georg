fdb02fb9fe580b2493e70fa1193e0da6c454c614
Ida Li <ida.li@nrm.se>
2020-04-21 09:33:13 +0200

    Update unit test

---
+++ georg-ui/tests/e2e/specs/reesultsView.js
@@ -0,0 +1,36 @@
+const visit = () => cy.visit("/");
+const fetchResults = () =>
+  cy
+    .focused()
+    .type("tyre")
+    .type("{enter}");
+
+describe("Results view", () => {
+  before(visit);
+  before(fetchResults);
+
+  it("Hover result", () => {
+    cy.get("#resultList")
+      .children()
+      .first()
+      .next()
+      .next()
+      .next()
+      .trigger("mouseover");
+  });
+  it("Icon click to highligt result", () => {
+    cy.get("#resultList")
+      .children()
+      .get("#iconBtn")
+      .click();
+  });
+  // it("Result click to display detail", () => {
+  //   cy.get("#resultList")
+  //     .children()
+  //     .first()
+  //     .get("#resultContent")
+  //     .children()
+  //     .click();
+  //   // cy.contains("TILLBAKA TRÄFFLISTAN").should("be.visible");
+  // });
+});
