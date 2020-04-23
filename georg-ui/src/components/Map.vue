7301303156e7c0cf49c3d7ee467d8cc4c0e9a94c
Ida Li <ida.li@nrm.se>
2020-04-23 10:08:17 +0200

    Disable add marker button when in detail view

--- georg-ui/src/components/Map.vue
+++ georg-ui/src/components/Map.vue
@@ -7,6 +7,7 @@
       @ready="getMapBounds"
       :style="mapHeight"
       @click="onMapClick"
+      :zoom="zoom"
     >
       <l-tile-layer
         url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
@@ -18,7 +19,6 @@
         :key="marker.id"
         :visible="marker.visible"
         :lat-lng="marker.position"
-        :accuracy="marker.accuracy"
         :icon="marker.icon"
         @click="onMarkerClick(marker.id)"
       ></l-marker>
@@ -41,6 +41,7 @@
         id="iconbtn"
         @click="enableAddMarker"
         style="cursor: pointer;"
+        :disabled="detailView"
       >
         <v-icon :color="iconColor">mdi-map-marker</v-icon>
       </v-btn>
@@ -64,7 +65,7 @@ const MAP_ICONS = {
   }),
   yellowIcon: L.icon({
     iconUrl: "http://maps.google.com/mapfiles/ms/micons/yellow-dot.png",
-    iconSize: [30, 30] // size of the icon
+    iconSize: [30, 30]
   })
 };
 
@@ -89,7 +90,8 @@ export default {
       },
       markers: [],
       enableAddMapMarkers: false,
-      circle: {}
+      circle: {},
+      zoom: 0
     };
   },
   mounted() {
@@ -102,6 +104,7 @@ export default {
   computed: {
     ...mapGetters([
       "detailView",
+      "didSearch",
       "results",
       "hovedResultId",
       "unhovedResultId",
@@ -117,13 +120,24 @@ export default {
     detailView: function() {
       this.$nextTick(() => {
         this.createMarks();
-        this.getMapBounds();
+        if (!this.detailView) {
+          this.getMapBounds();
+        }
+      });
+    },
+    center: function() {
+      this.$nextTick(() => {
+        this.$refs.myMap.mapObject.flyTo(
+          [this.center[0], this.center[1]],
+          this.zoom
+        );
       });
     },
     results: function() {
       this.$nextTick(() => {
         this.createMarks();
         this.getMapBounds();
+        // this.addActiveMarker();
       });
     },
     hovedResultId() {
@@ -140,6 +154,13 @@ export default {
       this.$nextTick(() => {
         this.removeOldSelectedMarker();
       });
+    },
+    didSearch() {
+      this.$nextTick(() => {
+        if (!this.detailView && !this.didSearch) {
+          this.enableAddMapMarkers = false;
+        }
+      });
     }
   },
   methods: {
@@ -149,21 +170,103 @@ export default {
       "setResults",
       "setDidSearch"
     ]),
+    addActiveMarker() {
+      this.$refs.myMap.mapObject.createPane("locationMarker");
+      this.$refs.myMap.mapObject.getPane("locationMarker").style.zIndex = 999;
+      let topMarker = L.marker([59.203241, 18.341203], {
+        pane: "locationMarker"
+      });
+      topMarker.addTo(this.$refs.myMap.mapObject);
+      topMarker.valueOf()._icon.style.filter = "hue-rotate(180deg)";
+    },
+    // createMarkers() {
+    //   if (this.detailView) {
+    //     const lat = this.selectedResult.geometry.coordinates[1];
+    //     const lon = this.selectedResult.geometry.coordinates[0];
+    //     this.center = [lat, lon];
+
+    //     let detailMarker = L.marker([lat, lon]);
+    //     detailMarker.addTo(this.$refs.myMap.mapObject);
+    //     // detailMarker.valueOf()._icon.style.filter = "hue-rotate(1E90FF)";
+    //     detailMarker.valueOf()._icon.style.filter = "hue-rotate(2.62rad)";
+    //     this.zoom = 18;
+    //     this.markers.push(detailMarker);
+    //   } else {
+    //     let north = 90;
+    //     let south = -90;
+    //     let west = -180;
+    //     let east = 180;
+
+    //     this.results.forEach(result => {
+    //       const lat = result.geometry.coordinates[1];
+    //       const lon = result.geometry.coordinates[0];
+    //       north = north > lat ? lat : north;
+    //       south = south > lat ? south : lat;
+    //       west = west > lon ? west : lon;
+    //       east = east > lon ? lon : east;
+
+    //       let southWest = new L.LatLng(south + 1, west + 1);
+    //       let northEast = new L.LatLng(north - 1, east - 1);
+    //       this.bounds = new L.LatLngBounds(southWest, northEast);
+
+    //       this.$refs.myMap.mapObject.createPane("top");
+    //       this.$refs.myMap.mapObject.getPane("top").style.zIndex = 999;
+    //       // let topMarker = L.marker([59.203241, 18.341203], {
+    //       //   pane: "top"
+    //       // });
+    //       // topMarker.addTo(this.$refs.myMap.mapObject);
+    //       // topMarker.valueOf()._icon.style.filter = "hue-rotate(180deg)";
+    //       let marker;
+    //       let icon;
+    //       if (result.properties.id === "newMarker") {
+    //         marker = L.marker([lat, lon]);
+    //         marker.addTo(this.$refs.myMap.mapObject);
+    //         marker.valueOf()._icon.style.filter = "hue-rotate(2.62rad)";
+    //       } else if (result.properties.id === this.selectedResultId) {
+    //         icon = MAP_ICONS.yellowIcon;
+    //         marker = {
+    //           id: result.properties.id,
+    //           position: [lat, lon],
+    //           accuracy: 100,
+    //           visible: true,
+    //           icon: icon,
+    //           pane: "top"
+    //         };
+    //       } else {
+    //         icon = MAP_ICONS.blueIcon;
+    //         marker = {
+    //           id: result.properties.id,
+    //           position: [lat, lon],
+    //           accuracy: 100,
+    //           visible: true,
+    //           icon: icon
+    //         };
+    //       }
+
+    //       // array.push(marker);
+    //     });
+    //   }
+    // },
     createMarks() {
       const array = [];
       if (this.detailView) {
         const lat = this.selectedResult.geometry.coordinates[1];
         const lon = this.selectedResult.geometry.coordinates[0];
 
-        this.enableAddMapMarkers = false;
+        // var greenIcon = L.icon({
+        //   iconUrl: "red-marker.png",
+        //   iconSize: [20, 30] // size of the icon
+        //   // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        // });
         this.center = [lat, lon];
         let marker = {
           id: this.selectedResult.properties.id,
           position: [lat, lon],
           visible: true,
-          icon: MAP_ICONS.blueIcon
+          icon: MAP_ICONS.yellowIcon
         };
         array.push(marker);
+        this.zoom = 18;
       } else {
         let north = 90;
         let south = -90;
@@ -182,15 +285,23 @@ export default {
           let northEast = new L.LatLng(north - 1, east - 1);
           this.bounds = new L.LatLngBounds(southWest, northEast);
 
-          let icon =
-            result.properties.id === "newMarker" ||
-            result.properties.id === this.selectedResultId
-              ? MAP_ICONS.redIcon
-              : MAP_ICONS.blueIcon;
+          // let topMarker = L.marker([59.203241, 18.341203], {
+          //   pane: "top"
+          // });
+          // topMarker.addTo(this.$refs.myMap.mapObject);
+          // topMarker.valueOf()._icon.style.filter = "hue-rotate(180deg)";
+
+          let icon;
+          if (result.properties.id === "newMarker") {
+            icon = MAP_ICONS.redIcon;
+          } else if (result.properties.id === this.selectedResultId) {
+            icon = MAP_ICONS.yellowIcon;
+          } else {
+            icon = MAP_ICONS.blueIcon;
+          }
           let marker = {
             id: result.properties.id,
             position: [lat, lon],
-            accuracy: 100,
             visible: true,
             icon: icon
           };
@@ -203,7 +314,7 @@ export default {
       const id = this.hovedResultId;
       this.markers.forEach(marker => {
         if (marker.id === id) {
-          marker.icon = MAP_ICONS.redIcon;
+          marker.icon = MAP_ICONS.yellowIcon;
         }
       });
     },
@@ -211,7 +322,7 @@ export default {
       const id = this.unhovedResultId;
       if (id != this.selectedResultId) {
         this.markers.forEach(marker => {
-          if (!marker.id.includes("newMarker")) {
+          if (marker.id != "newMarker") {
             if (marker.id == id) {
               marker.icon = MAP_ICONS.blueIcon;
             }
@@ -221,10 +332,7 @@ export default {
     },
     removeOldSelectedMarker() {
       this.markers.forEach(marker => {
-        if (
-          marker.id != this.selectedResultId &&
-          !marker.id.includes("newMarker")
-        ) {
+        if (marker.id != this.selectedResultId && marker.id != "newMarker") {
           marker.icon = MAP_ICONS.blueIcon;
         }
       });
@@ -252,14 +360,7 @@ export default {
           },
           name: "Din plats"
         };
-        // let marker = {
-        //   id: result.properties.id,
-        //   position: latlng,
-        //   visible: true,
-        //   icon: MAP_ICONS.redIcon
-        // };
-        // this.markers.push(marker);
-        // this.setNewMarkers(marker);
+
         let removeFirstResult = false;
         this.results.forEach(result => {
           if (result.properties.id == "newMarker") {
