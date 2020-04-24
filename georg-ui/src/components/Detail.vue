7d3230359d183e46a4e3291298d4ed3ada3415be
Ida Li <ida.li@nrm.se>
2020-04-24 08:50:54 +0200

    Auto fix lint error

--- georg-ui/src/components/Detail.vue
+++ georg-ui/src/components/Detail.vue
@@ -1,54 +1,68 @@
 <template>
   <v-card class="mt-2" width="400" id="v-card-detail">
-    <v-card-title  class="headline blue--text text--darken-2">
+    <v-card-title class="headline blue--text text--darken-2">
       {{ selectedResult.properties.name }}
     </v-card-title>
-    <v-card-subtitle><strong class="text-capitalize">{{ selectedResult.properties.layer }}</strong> enligt Who's On First</v-card-subtitle>
-        <v-list>
-          <v-list-item>
-              <v-list-item-icon>
-                  <v-icon color="blue darken-2">mdi-map-marker</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                  <v-list-item-title>{{ latLonDms }}</v-list-item-title>
-                  <v-list-item-subtitle>WGS84 DMS</v-list-item-subtitle>
-              </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-              <v-list-item-action></v-list-item-action>
-              <v-list-item-content>
-                  <v-list-item-title>{{ latLon }}</v-list-item-title>
-                  <v-list-item-subtitle>WGS84 DD</v-list-item-subtitle>
-              </v-list-item-content>
-          </v-list-item>
-          <v-divider inset></v-divider>
-          <v-list-item>
-              <v-list-item-icon>
-                  <v-icon color="blue darken-2">mdi-file-tree</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                  <v-list-item-title>{{selectedResult.properties.county}} <span class="text--secondary">county</span></v-list-item-title>
-                  <v-list-item-title>{{selectedResult.properties.region}} <span class="text--secondary">region</span></v-list-item-title>            
-                  <v-list-item-title>{{selectedResult.properties.country}} <span class="text--secondary">country</span></v-list-item-title> 
-              </v-list-item-content>
-          </v-list-item>
-          <v-divider inset></v-divider>
-          <v-list-item>
-              <v-list-item-icon>
-                <v-icon color="blue darken-2">mdi-database-import</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                  <v-list-item-title>Data från Who's On First (WOF)</v-list-item-title>
-              </v-list-item-content>
-              <v-btn
-                icon
-                href="https://whosonfirst.org/docs/licenses/"
-                target="_blank"
-              >
-                <v-icon>mdi-open-in-new</v-icon>
-              </v-btn>                 
-          </v-list-item>
-        </v-list>
+    <v-card-subtitle
+      ><strong class="text-capitalize">{{
+        selectedResult.properties.layer
+      }}</strong>
+      enligt Who's On First</v-card-subtitle
+    >
+    <v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue darken-2">mdi-map-marker</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ latLonDms }}</v-list-item-title>
+          <v-list-item-subtitle>WGS84 DMS</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-action></v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ latLon }}</v-list-item-title>
+          <v-list-item-subtitle>WGS84 DD</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider inset></v-divider>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue darken-2">mdi-file-tree</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title
+            >{{ selectedResult.properties.county }}
+            <span class="text--secondary">county</span></v-list-item-title
+          >
+          <v-list-item-title
+            >{{ selectedResult.properties.region }}
+            <span class="text--secondary">region</span></v-list-item-title
+          >
+          <v-list-item-title
+            >{{ selectedResult.properties.country }}
+            <span class="text--secondary">country</span></v-list-item-title
+          >
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider inset></v-divider>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue darken-2">mdi-database-import</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Data från Who's On First (WOF)</v-list-item-title>
+        </v-list-item-content>
+        <v-btn
+          icon
+          href="https://whosonfirst.org/docs/licenses/"
+          target="_blank"
+        >
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 
