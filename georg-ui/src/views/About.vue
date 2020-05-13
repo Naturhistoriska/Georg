f8c5d3dabaec2f796df7ed269f70cdb7ca2ccd91
Ida Li <ida.li@nrm.se>
2020-05-13 11:19:45 +0200

    Get version from package.json and add to about page

--- georg-ui/src/views/About.vue
+++ georg-ui/src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <h2>version: {{ version }}</h2>
   </div>
 </template>
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      version: 0,
+    }
+  },
+
+  mounted() {
+    this.setVersion()
+  },
+  methods: {
+    setVersion() {
+      this.version = process.env.VUE_APP_VERSION
+    },
+  },
+}
+</script>
