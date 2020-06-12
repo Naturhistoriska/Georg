ff03ae4883d643e0f784b513e372db4536d2eb5d
Ida Li <ida.li@nrm.se>
2020-06-12 10:31:11 +0200

    Change api endpoint

--- georg-ui/src/Service.js
+++ georg-ui/src/Service.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const baseUrl = process.env.VUE_APP_GEORG_API
 export default class Service {
   async fetchAddressResults(address) {
-    const url = `${baseUrl}geoCoding?address=${address}`
+    // const url = `${baseUrl}geoCoding?address=${address}`
+    const url = `${baseUrl}search?text=${address}`
     const response = await axios.get(url)
     return response.data
   }
