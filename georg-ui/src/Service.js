8c8d31247c2bf9c853ac2af9920e1949828cfaba
Ida Li <ida.li@nrm.se>
2020-06-13 11:18:47 +0200

    Add gbif occurrence api into service

--- georg-ui/src/Service.js
+++ georg-ui/src/Service.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
 
 const baseUrl = process.env.VUE_APP_GEORG_API
+
 export default class Service {
   async fetchAddressResults(address) {
-    // const url = `${baseUrl}geoCoding?address=${address}`
-    const url = `${baseUrl}search?text=${address}`
+    const url = `${baseUrl}geoCoding?address=${address}`
+    // const url = `${baseUrl}search?text=${address}`
+    const response = await axios.get(url)
+    return response.data
+  }
+
+  async fetchOccurrenceKey(dataset, occurrenceId) {
+    const url = `${baseUrl}occurancekey?dataset=${dataset}&occurrenceid=${occurrenceId}`
     const response = await axios.get(url)
     return response.data
   }
