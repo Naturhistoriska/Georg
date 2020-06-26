7cfda90c299b5bd6e444db4daea01c7fbfd0f8a4
Ida Li <ida.li@nrm.se>
2020-06-26 13:09:21 +0200

    Add reverseGeoCoding service

--- georg-ui/src/Service.js
+++ georg-ui/src/Service.js
@@ -1,13 +1,18 @@
 import axios from 'axios'
 
 const baseUrl = process.env.VUE_APP_GEORG_API
-// const gbifUrl = process.env.VUE_APP_GBIF_OCCURRENCE
-// const gbifDatasetApiUrl = process.env.VUE_APP_GBIF_DATASET_API
 const gbifApi = process.env.VUE_APP_GBIF_API
 
 export default class Service {
   async fetchAddressResults(address) {
     const url = `${baseUrl}geoCoding?address=${address}`
+    // const url = `${baseUrl}search?text=${address}`
+    const response = await axios.get(url)
+    return response.data
+  }
+
+  async reverseGeoCodingResults(lat, lng) {
+    const url = `${baseUrl}reverse?lat=${lat}&lng=${lng}`
     // const url = `${baseUrl}search?text=${address}`
     const response = await axios.get(url)
     return response.data
