47753ed8c0f608554e4d689ce9e5fb6ebd9a8f3a
Ida Li <ida.li@nrm.se>
2020-09-08 10:42:00 +0200

    Add multiple coordinates formats into response json

--- georg-service/src/main/java/se/nrm/georg/service/logic/GeorgLogic.java
+++ georg-service/src/main/java/se/nrm/georg/service/logic/GeorgLogic.java
@@ -25,14 +25,12 @@ public class GeorgLogic implements Serializable {
   @Inject
   private CoordinatesJson coordinates; 
   
-  private String peliasPath;  
-//  private final String secondSign = "\"";
+  private String peliasPath;   
   private final String comma = ",";
   private final String degreeSign = "°";
   private final String minuteSign = "'";
   
-  private final String emptySpace = " "; 
-  private final String emptyString = "";
+  private final String emptySpace = " ";  
    
   public GeorgLogic() {
     
@@ -48,60 +46,60 @@ public class GeorgLogic implements Serializable {
   public void init() {
     peliasPath = props.getPeliasPath();    
   }
-  
+    
   public String searchAddress(String address, String source, String layer, 
           String countryCode, int size) {
     log.info("searchAddress");
     
     String peliasUrl = Util.getInstance()
             .buildGeoCodePath(peliasPath, address, source, layer, countryCode, size);  
-     
-    String results = service.getResults(peliasUrl); 
-    return coordinates.addCoordinatesTransformation(results); 
+      
+    return coordinates.addCoordinatesTransformation(service.getResults(peliasUrl)); 
   }
   
   public String reverseSearch(double lat, double lon) {
     log.info("reverseSearch");
     String peliasUrl = Util.getInstance().buildReverseGeoCodePath(peliasPath, lat, lon);
-    
-    return service.getResults(peliasUrl);  
+   
+    return coordinates.addCoordinatesTransformation(service.getResults(peliasUrl)); 
   }
     
   public String runAutocompleteSearch(String text, String sources, String layers,
           String countryCode, int size) {
     log.info("runAutocompleteSearch");
     String searchUrl = Util.getInstance()
-            .bunildAutoCompleteSearchPath(peliasPath, text, sources, layers, countryCode, size);  
-    String results = service.getResults(searchUrl); 
-    return coordinates.addCoordinatesTransformation(results); 
+            .bunildAutoCompleteSearchPath(peliasPath, text, sources, layers, countryCode, size);   
+    return coordinates.addCoordinatesTransformation(service.getResults(searchUrl)); 
   } 
   
-  public String coordinatesSearch(String coordinates) {
+  public String coordinatesSearch(String coordinatesString) {
     double lat = 0;
     double lng = 0;
     
-    coordinates = coordinates.replace(comma, emptySpace);
-    if(CoordinatesHelper.getInstance().isDD(coordinates)) {
-      String[] coors = coordinates.split(emptySpace);
+    coordinatesString = coordinatesString.replace(comma, emptySpace);
+    if(CoordinatesHelper.getInstance().isDD(coordinatesString)) {
+      String[] coors = coordinatesString.split(emptySpace);
       lat = Double.valueOf(coors[0]); 
       lng = coors.length == 2 ? Double.valueOf(coors[1]) : Double.valueOf(coors[2]);
     } else {
-      String strLat = CoordinatesHelper.getInstance().getLatString(coordinates.toLowerCase());
-      String strLng = CoordinatesHelper.getInstance().getlngString(coordinates.toLowerCase());
+      String strLat = CoordinatesHelper.getInstance().getLatString(coordinatesString.toLowerCase());
+      String strLng = CoordinatesHelper.getInstance().getlngString(coordinatesString.toLowerCase());
        
-      if (CoordinatesHelper.getInstance().isDMS(coordinates)) {
-        lat = convertDMSToDD(strLat, coordinates.toLowerCase().contains("s"));
-        lng = convertDMSToDD(strLng, coordinates.toLowerCase().contains("w"));
-      } else if (CoordinatesHelper.getInstance().isDDM(coordinates)) {
-        lat = convertDDMToDD(strLat, coordinates.toLowerCase().contains("s"));
-        lng = convertDDMToDD(strLng, coordinates.toLowerCase().contains("w")); 
+      if (CoordinatesHelper.getInstance().isDMS(coordinatesString)) {
+        lat = convertDMSToDD(strLat, coordinatesString.toLowerCase().contains("s"));
+        lng = convertDMSToDD(strLng, coordinatesString.toLowerCase().contains("w"));
+      } else if (CoordinatesHelper.getInstance().isDDM(coordinatesString)) {
+        lat = convertDDMToDD(strLat, coordinatesString.toLowerCase().contains("s"));
+        lng = convertDDMToDD(strLng, coordinatesString.toLowerCase().contains("w"));  
       } 
     } 
     
     String peliasUrl = Util.getInstance().buildReverseGeoCodePath(peliasPath, lat, lng); 
 
 //    String peliasUrl = "https://georg-test.nrm.se/api/reverse?lat=" + lat + "&lng="+ lng;
-    return service.getResults(peliasUrl);  
+//    return service.getResults(peliasUrl);  
+     
+    return coordinates.addCoordinatesTransformation(service.getResults(peliasUrl)); 
   }
   
   private double convertDDMToDD(String value, boolean isSouthOrWest) {
