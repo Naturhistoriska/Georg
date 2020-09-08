f29352c7a80cb9a88d6f2195676b94eb788eedc1
Lisa Sundström <lisasund@users.noreply.github.com>
2020-09-08 13:58:47 +0200

    Move geografic levels to separate list items

--- georg-ui/src/components/GeographicTree.vue
+++ georg-ui/src/components/GeographicTree.vue
@@ -1,23 +1,35 @@
 <template>
-  <v-list-item>
-    <v-list-item-icon>
-      <v-icon v-bind:color="treeIconColor">mdi-file-tree</v-icon>
-    </v-list-item-icon>
-    <v-list-item-content>
-      <v-list-item-title>
-        {{ selectedResult.properties.county }}
-        <span class="text--secondary">county</span>
-      </v-list-item-title>
-      <v-list-item-title>
-        {{ selectedResult.properties.region }}
-        <span class="text--secondary">region</span>
-      </v-list-item-title>
-      <v-list-item-title>
-        {{ selectedResult.properties.country }}
-        <span class="text--secondary">country</span>
-      </v-list-item-title>
-    </v-list-item-content>
-  </v-list-item>
+  <v-list-item-group>
+    <v-list-item dense class="geotree">
+      <v-list-item-icon>
+        <v-icon v-bind:color="treeIconColor">mdi-file-tree</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ selectedResult.properties.county }}
+          <span class="text--secondary">county</span>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item dense class="geotree">
+      <v-list-item-icon></v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ selectedResult.properties.region }}
+          <span class="text--secondary">region</span>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item dense class="geotree">
+      <v-list-item-icon></v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ selectedResult.properties.country }}
+          <span class="text--secondary">country</span>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list-item-group>
 </template>
 <script>
 import { mapGetters } from 'vuex'
@@ -32,3 +44,13 @@ export default {
   },
 }
 </script>
+<style>
+.geotree.v-list-item--dense {
+  min-height: 40px !important;
+  max-height: 40px !important;
+}
+.geotree.v-list-item--dense .v-list-item__title {
+  font-size: 1rem !important;
+  font-weight: 400 !important;
+}
+</style>
