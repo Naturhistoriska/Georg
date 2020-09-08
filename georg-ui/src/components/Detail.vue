c5ff2c22dc81e86e542c8c7b7ac97110d0ffbbb8
Lisa Sundström <lisasund@users.noreply.github.com>
2020-09-08 13:46:38 +0200

    Move and separate show/copy JSON to chips, move WGS84 alert to below datasource

--- georg-ui/src/components/Detail.vue
+++ georg-ui/src/components/Detail.vue
@@ -1,14 +1,7 @@
 <template>
   <v-card class="mt-2" width="400" id="v-card-detail">
-    <v-card-title v-bind:class="titleClass" class="ma-0 pa-0 pl-4 pr-2">
-      <v-list-item class="ma-0 pa-0">
-        <v-list-item-content>{{ title }}</v-list-item-content>
-        <v-list-item-icon>
-          <v-btn icon v-clipboard="jsonString" @click.stop="dialog = true">
-            <v-icon small>mdi-content-copy</v-icon>
-          </v-btn>
-        </v-list-item-icon>
-      </v-list-item>
+    <v-card-title class="pl-4 pr-2 ">
+      {{ title }}
     </v-card-title>
     <v-card-subtitle v-if="isGbif">{{
       selectedResult.properties.name
@@ -19,11 +12,51 @@
       }}</strong>
       enligt {{ source }}
     </v-card-subtitle>
-    <v-card-text v-if="isGbif">
-      <v-alert dense text type="warning" class="alertText mb-n3 mt-0"
-        >Saknar geodetiskt datum, WGS84 har antagits.</v-alert
+    <v-divider></v-divider>
+    <v-card-text class="pb-3 pt-3">
+      <v-chip
+        color="grey darken-2"
+        outlined
+        class="mr-2"
+        tabindex="0"
+        @click.stop="dialog = true"
+        @keypress.stop="dialog = true"
       >
+        <!--Display dialog and snackbar should be fixed properly so that both works for clicking and pressing enter.-->
+        <v-icon left>mdi-code-tags</v-icon>
+        Visa JSON
+      </v-chip>
+      <v-chip
+        color="grey darken-2"
+        outlined
+        class="mr-2"
+        tabindex="0"
+        v-clipboard="jsonString"
+        @click.stop="snackbar = true"
+        @keypress="snackbar = true"
+      >
+        <!--Copy and snackbar should be fixed properly so that both works for clicking and pressing enter.-->
+        <v-snackbar v-model="snackbar" centered :timeout="600">
+          Platsens JSON har kopierats till Urklipp</v-snackbar
+        >
+        <v-icon left>mdi-content-copy</v-icon>
+        Kopiera
+      </v-chip>
+      <!--
+        To be added later:
+        <v-chip
+        v-if="!isDinPlats"
+        color="grey darken-2"
+        outlined
+        class="mr-2"
+        @click=""
+        tabindex="0"
+      >
+        <v-icon left>mdi-map-marker-plus</v-icon>
+        Min plats
+      </v-chip>-->
     </v-card-text>
+    <v-divider></v-divider>
     <v-list flat>
       <Coordinates
         v-bind:lat="selectedResult.geometry.coordinates[1]"
@@ -35,9 +68,18 @@
       <v-divider inset></v-divider>
       <Uncertainty v-if="isNewMarker" />
       <GbifDataSourceLinks v-else-if="isGbif" />
+      <v-list-item v-if="isGbif">
+        <v-list-item-action></v-list-item-action>
+        <v-alert dense text type="warning" class="alertText"
+          >Källan saknar geodetiskt datum. <br />WGS84 har antagits.</v-alert
+        ></v-list-item
+      >
       <DataSourceLinks v-else />
     </v-list>
-    <v-dialog v-model="dialog" max-width="550"> <JsonResult /></v-dialog>
+    <v-dialog v-model="dialog" max-width="550"><JsonResult /></v-dialog>
+    <v-snackbar v-model="snackbar" centered :timeout="600">
+      Platsens JSON har kopierats till Urklipp</v-snackbar
+    >
   </v-card>
 </template>
 
@@ -65,6 +107,7 @@ export default {
     return {
       // jsonString: 'test',
       dialog: false,
+      snackbar: false,
     }
   },
 
