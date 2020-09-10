cd032b0ffa0119bad6823266f0044db1d5cc4863
Ida Li <ida.li@nrm.se>
2020-09-10 15:44:00 +0200

    Remove din plats from coordinates search

--- georg-ui/src/components/SearchCoordinates.vue
+++ georg-ui/src/components/SearchCoordinates.vue
@@ -50,55 +50,82 @@ export default {
       'setResults',
       'setSelectedResultId',
       'setSelectedResult',
+      'setSearchCountry',
+      'setSearchOption',
     ]),
     clearSearch() {
       this.setResults([])
       this.setDetailView(false)
       this.setSelectedResultId('')
+      this.setSelectedResult({})
       this.setMessage('')
-      this.setDisplayJsonData(false)
-      this.$router.push('/')
+      // this.setDisplayJsonData(false)
+      // this.setSearchCountry('SWE')
+      // this.setSearchOption('address')
+      if (this.$route.fullPath !== '/') {
+        this.$router.push('/')
+      }
     },
     search() {
       this.isLoading = true
+
       service
         .coordinatesSearch(this.coordinates)
         .then(response => {
-          const lat = response.geocoding.query['point.lat']
-          const lng = response.geocoding.query['point.lon']
-
-          const newResult = {
-            properties: {
-              id: 'newMarker',
-              name: 'Din plats',
-              isNew: true,
-            },
-            geometry: {
-              coordinates: [fixer.digits(lng), fixer.digits(lat)],
-            },
-          }
-
-          this.results = response.features
-          if (this.results.length > 0) {
+          if (response.error) {
+            const errMsg = response.error.msgKey
+            if (errMsg === 'Invalid coordinates') {
+              const msg =
+                "Invalid coordinates.\nCorrect coordinates ex:\nDMS: 57°46'7\" N 14°49'37\" E\nDDM: 57°46.113480' N 14°49.621740' E\nDD: 57.768558 14.827029"
+              this.setMessage(msg)
+            }
+            this.setResults([])
             this.setDetailView(false)
+            this.setSelectedResultId('')
+            this.setSelectedResult({})
           } else {
-            this.setSelectedResult(newResult)
-            this.setSelectedResultId('newMarker')
-            this.setDetailView(true)
-          }
-          const message =
-            this.results.length > 0
-              ? this.results.length + ' träffar'
-              : 'Sökningen gav inga träffar'
-          this.setMessage(message)
+            const lat = response.geocoding.query['point.lat']
+            const lng = response.geocoding.query['point.lon']
 
-          this.results.unshift(newResult)
-          this.setResults(this.results)
+            this.results = response.features
+            if (this.results.length === 0) {
+              const newResult = {
+                properties: {
+                  id: 'newMarker',
+                  name: 'Din plats',
+                  isNew: true,
+                },
+                geometry: {
+                  coordinates: [fixer.digits(lng), fixer.digits(lat)],
+                },
+              }
+              this.setSelectedResult(newResult)
+              this.setSelectedResultId('newMarker')
+              this.results.unshift(newResult)
+            } else {
+              this.results[0].properties.id = 'newMarker'
+              this.results[0].properties.name = 'Din plats'
+            }
+            this.setDetailView(this.results.length === 1)
+
+            if (this.results.length === 1) {
+              this.setSelectedResultId(this.results[0])
+              this.setSelectedResult(this.results[0])
+            }
+
+            const message =
+              this.results.length > 0
+                ? this.results.length + ' träffar'
+                : 'Sökningen gav inga träffar'
+            this.setMessage(message)
+            this.setResults(this.results)
+          }
         })
         .catch(function() {})
         .finally(() => {
           this.isLoading = false
         })
+
       this.$router.push(`/search?coordinates=${this.coordinates}`)
     },
   },
