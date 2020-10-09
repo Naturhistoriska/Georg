5d27a11c79497f3cfb2bf652a92cc4d633500d80
Lisa Sundström <lisasund@users.noreply.github.com>
2020-10-09 11:48:51 +0200

    Add example navigation drawer

--- georg-ui/src/App.vue
+++ georg-ui/src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="blue darken-2" dark dense>
+    <v-navigation-drawer app clipped>
+      <v-list>
+        <v-list-item nav link>
+          <v-list-item-content>
+            <v-list-item-title>
+              Om Georg
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list nav dense>
+          <v-list-item-group v-model="item" color="primary">
+          <v-list-item>
+            <v-list-item-content>            
+          <v-list-item-title>Kontakta oss</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left color="blue darken-2" dark dense>
       <v-toolbar-title class="title">
         <router-link id="home" class="white--text routerLink" to="/"
           >Georg</router-link
@@ -9,7 +30,7 @@
       <v-spacer></v-spacer>
       <v-toolbar-title class="title">
         <v-tabs color="white" right background-color="blue darken-2" optional>
-          <v-tab id="aboutLink" key="about" to="/about">Om Georg</v-tab>
+          <v-tab id="aboutLink" key="about" to="/om">Om Georg</v-tab>
         </v-tabs>
       </v-toolbar-title>
     </v-app-bar>
