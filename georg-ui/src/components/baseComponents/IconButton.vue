fe47866affce626e8c930edc5ee89942f1355b19
Ida Li <ida.li@nrm.se>
2020-10-12 14:10:46 +0200

    Create IconButton component

---
+++ georg-ui/src/components/baseComponents/IconButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn icon :href="externallink" target="_blank" id="externalLink">
+    <BaseIcon v-bind:name="iconPrepend" v-if="iconPrepend" />
+    {{ text }}
+    <BaseIcon v-bind:name="iconAppend" v-if="iconAppend" />
+  </v-btn>
+</template>
+
+<script>
+import BaseIcon from '../components/baseComponents/BaseIcon'
+export default {
+  name: 'IconButton',
+  components: {
+    BaseIcon,
+  },
+  props: ['href', 'iconPrepend', 'iconAppend', 'text'],
+}
+</script>
