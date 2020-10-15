ad5813321194103c99b1dd61f7fda45aa706b498
Ida Li <ida.li@nrm.se>
2020-10-15 08:38:57 +0200

    Add high geography tree for dinplats

--- georg-service/src/main/java/se/nrm/georg/service/logic/json/CoordinatesJson.java
+++ georg-service/src/main/java/se/nrm/georg/service/logic/json/CoordinatesJson.java
@@ -1,7 +1,7 @@
 package se.nrm.georg.service.logic.json;
- 
-import java.math.BigDecimal; 
-import java.math.RoundingMode; 
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -17,34 +17,37 @@ import se.nrm.georg.service.logic.coordinates.CoordinatesHelper;
  */
 @Slf4j
 public class CoordinatesJson {
-  
+
   @Inject
   private JsonParser parser;
   @Inject
-  private CoordinatesBuilder builder; 
-  
+  private CoordinatesBuilder builder;
+
   private final String featuresKey = "features";
   private final String geometryKey = "geometry";
   private final String coordinatesKey = "coordinates";
   private final String propertiesKey = "properties";
-  private final String nameKey = "name"; 
-  private final String idKey = "id"; 
+  private final String nameKey = "name";
+  private final String idKey = "id";
   private final String gidKey = "gid";
-  private final String typeKey = "type"; 
+  private final String typeKey = "type";
   private final String dinPlats = "Din plats";
   private final String newMarker = "newMarker";
   private final String feature = "Feature";
   private final String point = "Point";
-   
+  private final String countryKey = "country";
+  private final String regionKey = "region";
+  private final String countyKey = "county";
+
   private final String ddKey = "dd";
   private final String dmsKey = "dms";
   private final String ddmKey = "ddm";
   private final String rt90Key = "rt90";
   private final String sweref99Key = "sweref99";
   private final DecimalFormat df = new DecimalFormat("###.000000");
-    
+
   public CoordinatesJson() {
-    
+
   }
 
   public CoordinatesJson(JsonParser parser, CoordinatesBuilder builder) {
@@ -80,14 +83,9 @@ public class CoordinatesJson {
 
     JSONObject jsonObject = parser.convertStringToJson(jsonString);
     JSONArray array = parser.getJsonArray(jsonObject, featuresKey);
-    
-//    if (isNewMarker || array.length() == 0) {
-//      JSONObject dinPlatsJson = addDinPlats(roundDoubleToSix(lat), roundDoubleToSix(lng));
-//      array.put(dinPlatsJson);
-//    } else {
-
-    if (array.length() == 0 ) {
-      JSONObject dinPlatsJson = addDinPlats(roundDoubleToSix(lat), roundDoubleToSix(lng));
+ 
+    if (array.length() == 0) {
+      JSONObject dinPlatsJson = addDinPlats(null, roundDoubleToSix(lat), roundDoubleToSix(lng));
       array.put(dinPlatsJson);
     } else if (isNewMarker) {
 //      JSONObject dinPlatsJson = addDinPlats(roundDoubleToSix(lat), roundDoubleToSix(lng));
@@ -98,13 +96,12 @@ public class CoordinatesJson {
       property.put(idKey, newMarker);
       property.put(gidKey, newMarker);
       JSONObject geometryJson = firstJson.getJSONObject(geometryKey);
-      parser.buildJsonObject(geometryJson, coordinatesKey, lng, lat); 
+      parser.buildJsonObject(geometryJson, coordinatesKey, lng, lat);
       property.put(coordinatesKey, buildCoordinatesTransformationJson(roundDoubleToSix(lat), roundDoubleToSix(lng)));
-    } else {
+    } else { 
       array.iterator().forEachRemaining(element -> {
         JSONObject jsonObj = (JSONObject) element;
         JSONObject geometryJson = jsonObj.getJSONObject(geometryKey);
-
         JSONObject property = jsonObj.getJSONObject(propertiesKey);
 //        if(!isNewMarker) {
         JSONArray coordinates = parser.getJsonArray(geometryJson, coordinatesKey);
@@ -112,32 +109,39 @@ public class CoordinatesJson {
         double realLat = coordinates.getBigDecimal(1).setScale(6, RoundingMode.HALF_UP).doubleValue();
         parser.buildJsonObject(geometryJson, coordinatesKey, realLng, realLat);
         property.put(coordinatesKey, buildCoordinatesTransformationJson(roundDoubleToSix(realLat), roundDoubleToSix(realLng)));
-//        } else {
-//          parser.buildJsonObject(geometryJson, coordinatesKey, lng, lat);
-//          property.put(nameKey, dinPlats);
-//          property.put(idKey, newMarker);
-//          property.put(gidKey, newMarker);
-//          property.put(coordinatesKey, buildCoordinatesTransformationJson(roundDoubleToSix(lat), roundDoubleToSix(lng)));
-//        } 
 
-//        if (isNewMarker) {
-//          property.put(nameKey, dinPlats);
-//          property.put(idKey, newMarker);
-//          property.put(gidKey, newMarker);
-//        }
-//        property.put(coordinatesKey, buildCoordinatesTransformationJson(roundDoubleToSix(realLat), roundDoubleToSix(realLng)));
       });
-      if (!isNewMarker) {
-        JSONObject dinPlatsJson = addDinPlats(roundDoubleToSix(lat), roundDoubleToSix(lng));
-        array.put(dinPlatsJson);
-      }
-    } 
+
+      JSONObject dinPlatsJson = addDinPlats(array.getJSONObject(0).getJSONObject(propertiesKey), roundDoubleToSix(lat), roundDoubleToSix(lng)); 
+      array.put(dinPlatsJson);
+
+
+    }
     return jsonObject.toString();
   }
 
-  private JSONObject addDinPlats(double lat, double lng) {
+//  private JSONObject addDinPlats(JSONObject json, double lat, double lng) {
+//    log.info("addDinPlats : {}");
+//
+//    String country = json.getJSONObject(propertiesKey).getString("country");
+//    String region = json.getJSONObject(propertiesKey).getString("region");
+//    String county = json.getJSONObject(propertiesKey).getString("county");
+//    JSONObject dinPlatsJson = addDinPlats(lat, lng);
+//
+//   
+//      dinPlatsJson.put(geometryKey, json.getJSONObject(geometryKey)); 
+//
+// 
+//
+//    return dinPlatsJson;
+//  }
+
+  private JSONObject addDinPlats(JSONObject json, double lat, double lng ) {
+     
+
+    
     log.info("addDinPlats : {}");
-    JSONObject json = new JSONObject(); 
+    JSONObject dinPlatsJson = new JSONObject();
     JSONObject geometryJson = new JSONObject();
     parser.buildJsonObject(geometryJson, coordinatesKey, lng, lat);
     geometryJson.put(typeKey, point);
@@ -146,12 +150,26 @@ public class CoordinatesJson {
     propertiesJson.put(gidKey, newMarker);
     propertiesJson.put(nameKey, dinPlats);
     propertiesJson.put(coordinatesKey, buildCoordinatesTransformationJson(lat, lng));
- 
-    json.put(geometryKey, geometryJson);
-    json.put(typeKey, feature);
-    json.put(propertiesKey, propertiesJson);
+    if (json != null) {
+      String country = json.has(countryKey) ? json.getString(countryKey) : null;
+      String region = json.has(regionKey) ? json.getString(regionKey) : null;
+      String county = json.has(countyKey) ? json.getString(countyKey) : null;
+      if (country != null) {
+        propertiesJson.put(countryKey, country);
+      }
+      if (region != null) {
+        propertiesJson.put(regionKey, region);
+      }
+      if (county != null) {
+        propertiesJson.put(countyKey, county);
+      }
+    }
 
-    return json;
+    dinPlatsJson.put(geometryKey, geometryJson);
+    dinPlatsJson.put(typeKey, feature);
+    dinPlatsJson.put(propertiesKey, propertiesJson);
+
+    return dinPlatsJson;
   }
 
   private double roundDoubleToSix(Double val) {
@@ -170,7 +188,7 @@ public class CoordinatesJson {
       JSONObject geometry = parser.getJsonObject(jsonObj, geometryKey);
       JSONArray coordinates = parser.getJsonArray(geometry, coordinatesKey);
       BigDecimal lng = coordinates.getBigDecimal(0).setScale(6, RoundingMode.HALF_UP);
-      BigDecimal lat = coordinates.getBigDecimal(1).setScale(6, RoundingMode.HALF_UP); 
+      BigDecimal lat = coordinates.getBigDecimal(1).setScale(6, RoundingMode.HALF_UP);
       JSONObject property = jsonObj.getJSONObject(propertiesKey);
       property.put(coordinatesKey,
               buildCoordinatesTransformationJson(lat.doubleValue(), lng.doubleValue()));
