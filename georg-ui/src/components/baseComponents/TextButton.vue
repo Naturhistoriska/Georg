49dd36ed47816a4b0137347bf7710490e834a7aa
Ida Li <ida.li@nrm.se>
2020-10-15 22:19:35 +0200

    Create TextButton.vue

---
+++ georg-ui/src/components/baseComponents/TextButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn text small :class="className" @click="onclick">
+    <BaseIcon v-bind:name="iconPrepend" v-if="iconPrepend" />
+    {{ text }}
+    <BaseIcon v-bind:name="iconAppend" v-if="iconAppend" />
+  </v-btn>
+</template>
+<script>
+import BaseIcon from './BaseIcon'
+export default {
+  name: 'IconButton',
+  components: {
+    BaseIcon,
+  },
+  props: ['className', 'iconPrepend', 'iconAppend', 'text'],
+  methods: {
+    onclick() {
+      this.$emit('clicked')
+    },
+  },
+}
+</script>
+<style scoped>
+.v-btn {
+  text-transform: none !important;
+}
+</style>
