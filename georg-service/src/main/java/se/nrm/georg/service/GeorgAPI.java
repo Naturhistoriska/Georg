a8af9230d18fe8735ea504feadde768aa30f28cc
Ida Li <ida.li@nrm.se>
2020-10-21 08:36:57 +0200

    Change api version from 0.4.1 to 0.4.2

--- georg-service/src/main/java/se/nrm/georg/service/GeorgAPI.java
+++ georg-service/src/main/java/se/nrm/georg/service/GeorgAPI.java
@@ -5,14 +5,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.annotations.Tag;
-import javax.inject.Inject; 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import lombok.extern.slf4j.Slf4j; 
+import lombok.extern.slf4j.Slf4j;
 import se.nrm.georg.service.logic.GeorgLogic;
 import se.nrm.georg.service.logic.exceptions.ErrorMessageBuilder;
 
@@ -22,10 +22,10 @@ import se.nrm.georg.service.logic.exceptions.ErrorMessageBuilder;
  */
 @Path("/")
 @Api(tags = {"georg"})
-@SwaggerDefinition(   
+@SwaggerDefinition(
   info = @Info(
                 title = "Georg API",
-                version = "0.4.1"
+                version = "0.4.2"
         ),
   tags = {
           @Tag(name = "georg", description = "Georeference tool")
@@ -33,7 +33,7 @@ import se.nrm.georg.service.logic.exceptions.ErrorMessageBuilder;
 @Produces(MediaType.APPLICATION_JSON)
 @Slf4j
 public class GeorgAPI {
- 
+
   @Inject
   private GeorgLogic logic;
   @Inject
@@ -43,19 +43,19 @@ public class GeorgAPI {
   @GET
   @Path("/geoCoding")
   @ApiOperation(value = "Get geocoding",
-          notes = "Return search results in json", 
+          notes = "Return search results in json",
           response = String.class
   )
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getGeoCode(@QueryParam("address") String address,   
-          @QueryParam("source") String source, @QueryParam("layer") String layer, 
-          @QueryParam("countryCode") String countryCode, 
+  public Response getGeoCode(@QueryParam("address") String address,
+          @QueryParam("source") String source, @QueryParam("layer") String layer,
+          @QueryParam("countryCode") String countryCode,
           @QueryParam("size") int size) {
     log.info("getGeoCode: {}, {}", address, source);
 
     return Response.ok(logic.searchAddress(address, source, layer, countryCode, size)).build();
-  } 
-  
+  }
+
   @GET
   @Path("/search")
   @ApiOperation(value = "Search",
@@ -63,16 +63,16 @@ public class GeorgAPI {
           response = String.class
   )
   @Produces(MediaType.APPLICATION_JSON)
-  public Response search(@QueryParam("text") String text, 
-          @QueryParam("sources") String sources, 
+  public Response search(@QueryParam("text") String text,
+          @QueryParam("sources") String sources,
           @QueryParam("layers") String layers,
-          @QueryParam("countryCode") String countryCode, 
+          @QueryParam("countryCode") String countryCode,
           @QueryParam("size") int size) {
     log.info("search: {}, {}", text, countryCode);
 
     return Response.ok(logic.runAutocompleteSearch(text, sources, layers, countryCode, size)).build();
-  } 
-   
+  }
+
   @GET
   @Path("/reverse")
   @ApiOperation(value = "Get reverse geocoding",
@@ -86,7 +86,7 @@ public class GeorgAPI {
 
     return Response.ok(logic.reverseSearch(lat, lon)).build();
   }
-  
+
   @GET
   @Path("/coordinates")
   @ApiOperation(value = "Search coordinates in different formate",
@@ -96,13 +96,13 @@ public class GeorgAPI {
   @Produces(MediaType.APPLICATION_JSON)
   public Response searchCoordinates(@QueryParam("coordinates") String coordinates) {
     log.info("searchCoordinates: {}", coordinates);
-    
+
     try {
       return Response.ok(logic.coordinatesSearch(coordinates)).build();
     } catch(NumberFormatException ex) {
       log.info(ex.getMessage());
-      return Response.ok(errorBuilder.buildInvalidCoordinatesMessage()).build(); 
+      return Response.ok(errorBuilder.buildInvalidCoordinatesMessage()).build();
     }
-    
+
   }
 }
