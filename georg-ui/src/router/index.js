65dfa0941ceddfa0c954c8fbf6b16460b646b09b
Ida Li <ida.li@nrm.se>
2020-11-12 15:20:09 +0100

    Add META to make page block indexing

--- georg-ui/src/router/index.js
+++ georg-ui/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+const Home = () => import('../views/Home.vue')
+const About = () => import('../views/About.vue')
+const Contact = () => import('../views/Contact.vue')
 
 Vue.use(VueRouter)
 
@@ -13,14 +16,16 @@ const routes = [
   {
     path: '/om',
     name: 'About',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: '/kontakt',
     name: 'Contact',
-    component: () =>
-      import(/* webpackChunkName: "contact" */ '../views/Contact.vue'),
+    component: Contact,
+    // component: () =>
+    //   import(/* webpackChunkName: "contact" */ '../views/Contact.vue'),
   },
   { path: '/search', name: 'Search', component: Home },
 ]
