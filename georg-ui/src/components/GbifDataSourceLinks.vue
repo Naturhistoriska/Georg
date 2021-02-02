fa01e9a8e536bb73b4700dc96dde388b3e11bccb
Ida Li <ida.li@nrm.se>
2021-02-02 08:43:11 +0100

    Fix error

--- georg-ui/src/components/GbifDataSourceLinks.vue
+++ georg-ui/src/components/GbifDataSourceLinks.vue
@@ -86,7 +86,7 @@ export default {
     },
   },
   methods: {
-    openOrCloseGbifData() {
+    openOrCloseGbifData(event) {
       event.preventDefault()
       this.displayGbifData = !this.displayGbifData
       this.iconToggleGbifData = this.displayGbifData
