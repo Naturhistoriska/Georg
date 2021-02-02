ae3a8a1318d6c4155d6f27a79af2d7d985ac8d4d
Ida Li <ida.li@nrm.se>
2021-02-02 08:41:03 +0100

    Create new component ActionIconButton

---
+++ georg-ui/src/components/baseComponents/ActionIconButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-btn
+    :class="btnClass"
+    :color="color"
+    :small="small"
+    icon
+    role="button"
+    @click="onClick"
+  >
+    <BaseIcon>{{ iconName }}</BaseIcon>
+  </v-btn>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon'
+export default {
+  name: 'IconButton',
+  components: {
+    BaseIcon,
+  },
+  props: ['btnClass', 'color', 'iconName', 'small'],
+  methods: {
+    onClick(event) {
+      event.preventDefault()
+      this.$emit('on-icon-click')
+    },
+  },
+}
+</script>
