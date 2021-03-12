e5ec84c5c7a7b870b9a787bcef6e22add458f42d
Ida Li <ida.li@nrm.se>
2021-03-12 10:08:49 +0100

    Make english as default language

--- georg-ui/src/i18n.js
+++ georg-ui/src/i18n.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 // import en from '@/locales/en.json'
-import sv from '@/locales/sv.json'
+import en from '@/locales/en.json'
 
 // const defaultImpl = VueI18n.prototype.getChoiceIndex
 
@@ -51,9 +51,9 @@ Vue.use(VueI18n)
 // }
 
 export const i18n = new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'sv',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'sv',
-  messages: { sv },
+  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  messages: { en },
 })
 
 // export default new VueI18n({
